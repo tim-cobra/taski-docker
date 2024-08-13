123d3cc55963e358547dabf42b52bf5f06a0021a
Артем Коберницкий <tim-cobra@mail.ru>
2024-08-14 00:32:25 +0300

    Finish_all16

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.89.208', '127.0.0.1', 'localhost', 'infra48.ddns.net']
+ALLOWED_HOSTS = ['158.160.89.208', '127.0.0.1', 'localhost', 'mytaska48.myvnc.com']
 
 
 # Application definition
